7071763baeaf80b0dcbaf7b66e0af94027f46c6f
Nothando <155914091+Mk3-thandow@users.noreply.github.com>
2025-09-03 15:28:30 +0200

    Add files via upload

---
+++ about-script.js
@@ -0,0 +1,25 @@
+function opentab(tabname, event) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+  }
+
+    var sidemenu = document.getElementById("sidemenu");
+
+    function openmenu(event){
+      sidemenu.style.right = "0"
+    }
+    function closemenu(event){
+      sidemenu.style.right = "-200px"
+    }
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
